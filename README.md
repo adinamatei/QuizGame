9687225fe14382fd8bf646c97218b1551e32294c
Adina Matei <adinamateim@gmail.com>
2018-11-02 16:00:04 +0200

    finishing the challenge

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # QuizGame
 JavaScript: Playing with objects and functions
+
+This is a coding challenge from Udemy course.
+
+[Demo](https://adinamatei.github.io/QuizGame/)
--- script.js
+++ script.js
@@ -22,6 +22,18 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
 
 // make the code private
 (function () {
@@ -43,16 +55,27 @@ c) correct answer (I would use a number for this)
     };
 
     // check the answer
-    Question.prototype.checkAnswer = function(ans) {
-        // check for answer
+    Question.prototype.checkAnswer = function(ans, callback) {
+        let theScore;
+
+        // check for answer and score
         if (ans === this.correctAnswer) {
             console.log("Correct answer!");
+            theScore = callback(true);
 
         } else {
-            console.log("Wrong answer!")
-
+            console.log("Wrong answer!");
+            theScore = callback(false);
         }
+        //display the score
+        this.displayScore(theScore);
     };
+
+    // display the score
+    Question.prototype.displayScore = function (score) {
+        console.log("Your score: " + score);
+        console.log("---------------------------");
+    }
 
     // Create the questions
     const question1 = "Is JavaScript case-sensitive?";
@@ -79,33 +102,44 @@ c) correct answer (I would use a number for this)
 
     // store the questions into array
     const arrQuestions = [q1, q2, q3, q4];
+    
+    // keep score 
+    function calcScore() {
+        const score = 0;
+        return function (correct) {
+            if(correct){
+                score += 1;
+            }
+            return score;
+        }
+    }
+    const keepScore = calcScore();
+    
+    // display next question
+    function nextQuestion() {
+        // get a random question
+        let randomQuestion = Math.floor(Math.random() * arrQuestions.length);
+        // console.log(randomQuestion);
+        // console.log(arrQuestions[randomQuestion]);
 
-    // get a random question
-    let randomQuestion = Math.floor(Math.random() * arrQuestions.length);
-    // console.log(randomQuestion);
-    // console.log(arrQuestions[randomQuestion]);
+        // display the question
+        arrQuestions[randomQuestion].logQuestion();
 
-    // display the question
-    arrQuestions[randomQuestion].logQuestion();
+        // ask the user for the correct answer
+        const userAnswer = prompt("Please select the correct answer (type a number).");
 
-    // ask the user for the correct answer
-    const userAnswer = parseInt(prompt("Please select the correct answer (type a number)."));
-    // check and display the answer
-    arrQuestions[randomQuestion].checkAnswer(userAnswer);
+        // type 'exit' if you want end the game
+        if(userAnswer !== 'exit'){
+            // check and display the answer
+            arrQuestions[randomQuestion].checkAnswer(parseInt(userAnswer), keepScore);
+            nextQuestion()
+        } else {
+            console.log('Game Over!')
+        }
+    }
 
-
+    nextQuestion()
 })();
 
 
-/*
---- Expert level ---
-
-8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
-
-9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
-
-10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
-
-11. Display the score in the console. Use yet another method for this.
-*/
 
